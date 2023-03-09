f49cec0d67b4a99618181106804889441ac86d35
B1129040 <119659262+B1129040@users.noreply.github.com>
2023-03-09 14:08:21 +0800

    Update main.c

--- Lab#1/main.c
+++ Lab#1/main.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main() {
-    int a[] = {1, 2, 3};
-    //ar b[] = "ABC";
-    //oat c[] = {1.1, 1.2, 1.3};
-    FILE* fp;
-    int arr_read[3];
-    
-    fp = fopen("a.bin", "wb+");
-    fwrite (a, sizeof(a), 1 ,fp);
-    fseek (fp, 0, SEEK_SET);
-    fread (arr_read,sizeof(a),1,fp);
-    
-    for(int i =0;i<3;i++) {
-        printf("%d",arr_read[i]);
-    }
-    fclose(fp);
-    
-    return 0;
-}
+int main(){
+	int a[]={1,2,3}, aa[3],i;
+	char b[]={"ABC"};
+	float c[]={1.1,1.2,1.3};
+	FILE *f;
+	f=fopen("a.bin","wb");
+	fwrite(a,sizeof(int),3,f);
+	fread(aa,sizeof(int),3,f);
+	for(i=0;i<3;i++)
+		printf("%d ",aa[i]);
+	fclose(f);
+	return 0;
+} 
